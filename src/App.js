e6bfd6837e236430aa968bf10ccbac02235a9c65
BomEllen <jjjj200100@gmail.com>
2023-11-20 09:40:16 +0900

    일단은 fullcalendar 연동

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    ' '
- );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <FullCalendar
+        defaultView='dayGridMonth'
+        plugins={[ dayGridPlugin ]}
+      />
+      </div>
+    );
+  }
 }
 
 export default App;
